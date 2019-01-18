82ed5b34b7d7e9316d466defbb0b860088285f44
Arthur Pisakhov <arthurpisakhov1@gmail.com>
2019-01-18 16:37:48 -0500

    remove post sharing

--- src/components/PostShare/PostShare.jsx
+++ src/components/PostShare/PostShare.jsx
@@ -28,37 +28,7 @@ class PostShare extends React.Component {
 
     return (
       <section className="share">
-        <h4>Share this post</h4>
-        <div style={{ display: "flex" }}>
-          <TwitterShareButton url={url} title={post.title}>
-            <a className="icon-twitter" style={{ fontSize: "1.4em" }}>
-              <span className="hidden">Twitter</span>
-            </a>
-          </TwitterShareButton>
-          <FacebookShareButton url={url} quote={post.title}>
-            <a className="icon-facebook" style={{ fontSize: "1.4em" }}>
-              <span className="hidden">Facebook</span>
-            </a>
-          </FacebookShareButton>
-          {/* <PinterestShareButton */}
-          {/* url={url} */}
-          {/* media={formatSiteUrl( */}
-          {/* config.siteUrl, */}
-          {/* config.pathPrefix, */}
-          {/* getCover(post) */}
-          {/* )} */}
-          {/* description={postNode.excerpt} */}
-          {/* > */}
-          {/* <a className="icon-pinterest" style={{ fontSize: "1.4em" }}> */}
-          {/* <span className="hidden">Pinterest</span> */}
-          {/* </a> */}
-          {/* </PinterestShareButton> */}
-          <GooglePlusShareButton url={url}>
-            <a className="icon-google-plus" style={{ fontSize: "1.4em" }}>
-              <span className="hidden">Google+</span>
-            </a>
-          </GooglePlusShareButton>
-        </div>
+        
       </section>
     );
   }
